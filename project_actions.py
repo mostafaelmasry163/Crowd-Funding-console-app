fbab90703d6dcc944d682f682ebd774cc3884fe9
“OmarMahmoud” <omarmahmoudquraa@gmail.com>
2021-08-02 05:45:25 +0200

    start creat function builing

--- project_actions.py
+++ project_actions.py
@@ -1,3 +1,7 @@
+import openpyxl
+import os
+
+
 def asking_for_action(user_name):
     try:
         decision = int(input(f'please {user_name} choose what do you want to do : \n '
@@ -11,7 +15,7 @@ def asking_for_action(user_name):
         asking_for_action(user_name)
     else:
         if decision == 1:
-            create_project()
+            create_project(user_name)
         elif decision == 2:
             view_projects()
         elif decision == 3:
@@ -23,39 +27,43 @@ def asking_for_action(user_name):
             asking_for_action(user_name)
 
 
+def create_project(user_name):
+    my_workbook = openpyxl.load_workbook("projects_data.xlsx")
+    projects_sheet = my_workbook["Sheet1"]
+    i = 1
+    j = 1
+    # Getting project title and make sure that there is no similar title and it is not empty
+    title = input("please, enter project title :  ")
+    while (not title) or (projects_sheet.cell(j, 1).value is not None):
+        if not title:
+            print("title should have at least on character")
+        elif projects_sheet.cell(j, 1).value == title:
+            print("this title is already exist")
+            j += 1
+        title = input("please, enter project title :  ")
+    print(title)
 
+    details = input("please, enter project details :  ")
 
-
-def create_project():
+    while projects_sheet.cell(i, 6).value is not None:
+        # print(projects_sheet.cell(i, 6).value, i)
+        i += 1
+    projects_sheet.cell(i, 6).value = user_name
+    my_workbook.save("projects_data.xlsx")
     print("create omar")
-
-
-
-
 
 
 def view_projects():
     print("view")
 
 
-
-
-
-
-
-
 def delete_project():
     print("delete")
-
-
-
-
-
 
 
 def search_by_date():
     print("search")
 
 
-
 asking_for_action("omar")
+
