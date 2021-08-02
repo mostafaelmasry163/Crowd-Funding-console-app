878c8dd53dd3d315f82b3e14f9ee7fde4a19c4e3
“OmarMahmoud” <omarmahmoudquraa@gmail.com>
2021-08-02 22:03:48 +0200

    finish creation function

--- project_actions.py
+++ project_actions.py
@@ -1,21 +1,22 @@
 import openpyxl
 import os
+import datetime
 
 
-def asking_for_action(user_name):
+def asking_for_action(user_mail):
     try:
-        decision = int(input(f'please {user_name} choose what do you want to do : \n '
+        decision = int(input(f'please choose what do you want to do : \n '
                              f'Enter 1 for creating new project.\n'
                              f' Enter 2 for viewing all projects. \n'
                              f' Enter 3 for deleting one of your projects.\n'
-                             f' Enter 4 gor searching for a project by its date.\n'
+                             f' Enter 4 for searching for a project by its date.\n'
                              f' type here : '))
     except:
         print("invalid, try again ")
-        asking_for_action(user_name)
+        asking_for_action(user_mail)
     else:
         if decision == 1:
-            create_project(user_name)
+            create_project(user_mail)
         elif decision == 2:
             view_projects()
         elif decision == 3:
@@ -24,33 +25,100 @@ def asking_for_action(user_name):
             search_by_date()
         else:
             print("invalid, try again ")
-            asking_for_action(user_name)
+            asking_for_action(user_mail)
 
 
-def create_project(user_name):
+def create_project(user_mail):
+    # clearing console
+    os.system('clear')
+    # Open File
     my_workbook = openpyxl.load_workbook("projects_data.xlsx")
     projects_sheet = my_workbook["Sheet1"]
-    i = 1
-    j = 1
+
     # Getting project title and make sure that there is no similar title and it is not empty
+    j = 1
     title = input("please, enter project title :  ")
     while (not title) or (projects_sheet.cell(j, 1).value is not None):
         if not title:
-            print("title should have at least on character")
+            print("title should have at least one character")
+            j = 1
+            title = input("please, enter project title :  ")
         elif projects_sheet.cell(j, 1).value == title:
             print("this title is already exist")
+            j = 1
+            title = input("please, enter project title :  ")
+        else:
             j += 1
-        title = input("please, enter project title :  ")
-    print(title)
+    os.system('clear')
 
+    # Getting project details
     details = input("please, enter project details :  ")
+    os.system('clear')
 
+    # Get and validate project total target
+    total_target = input("please, enter project total target :  ")
+    while (not total_target) or (total_target.isdigit() is False):
+        if not total_target:
+            print("target should have at least one digit")
+            total_target = input("please, enter project total target :  ")
+        elif total_target.isdigit() is False:
+            print("project target can be only digits")
+            total_target = input("please, enter project total target :  ")
+    os.system('clear')
+
+    # Get campaign start date
+    user_start_date = input("please, enter project start date in form [Day/Month/Year] :  ")
+    condition = True
+    while condition:
+        try:
+            start_date_ob = datetime.datetime.strptime(user_start_date, "%d/%m/%Y")
+            condition = False
+            if start_date_ob.date() < datetime.datetime.now().date():
+                print("start date cannot be a previous date to today date ")
+                user_start_date = input("please, enter project start date in form [Day/Month/Year] :  ")
+                condition = True
+        except:
+            print("invalid formula")
+            user_start_date = input("please, enter project start date in form [Day/Month/Year] :  ")
+    os.system('clear')
+
+    # Get campaign start date
+    user_end_date = input("please, enter project end date in form [Day/Month/Year] :  ")
+    condition2 = True
+    while condition2:
+        try:
+            end_date_ob = datetime.datetime.strptime(user_end_date, "%d/%m/%Y")
+            condition2 = False
+            if end_date_ob.date() < start_date_ob.date():
+                print("end date cannot be a previous date to project start date ")
+                user_end_date = input("please, enter project end date in form [Day/Month/Year] :  ")
+                condition2 = True
+        except:
+            print("invalid formula")
+            user_end_date = input("please, enter project end date in form [Day/Month/Year] :  ")
+    os.system('clear')
+
+    # showing project data
+    os.system('clear')
+    print(f'your project information is : \n'
+          f'Project title : {title}  \n'
+          f'Project details : {details}  \n'
+          f'Project total target : {total_target}  \n'
+          f'Project start date and end date :  {user_start_date}  ,  {user_end_date}')
+
+    # writing data
+    i = 1
     while projects_sheet.cell(i, 6).value is not None:
-        # print(projects_sheet.cell(i, 6).value, i)
         i += 1
-    projects_sheet.cell(i, 6).value = user_name
+    projects_sheet.cell(i, 6).value = user_mail
+    projects_sheet.cell(i, 1).value = title
+    projects_sheet.cell(i, 2).value = details
+    projects_sheet.cell(i, 3).value = total_target
+    projects_sheet.cell(i, 4).value = user_start_date
+    projects_sheet.cell(i, 5).value = user_end_date
     my_workbook.save("projects_data.xlsx")
-    print("create omar")
+
+    print("End of creation")
 
 
 def view_projects():
@@ -64,6 +132,4 @@ def delete_project():
 def search_by_date():
     print("search")
 
-
-asking_for_action("omar")
 
